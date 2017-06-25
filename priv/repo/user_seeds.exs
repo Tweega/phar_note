4e87b6160a0d1475dfe2350f8407cd92e22ae60f
Christopher Roe <cpr@tweega.com>
2017-06-25 16:24:00 +0100

    added more users to user seeds.exs

--- priv/repo/user_seeds.exs
+++ priv/repo/user_seeds.exs
@@ -20,8 +20,22 @@ Repo.delete_all(User)
 Repo.insert! %User{ first_name: "Harry", last_name: "Potter", email: "hp@hogwarts.wiz", photo_url: "images/HP/harrypotter.jpg" }
 Repo.insert! %User{ first_name: "Hermione", last_name: "Granger", email: "hg@hogwarts.wiz", photo_url: "images/HP/hermione.jpg" }
 Repo.insert! %User{ first_name: "Ron", last_name: "Weasley", email: "rw@hogwarts.wiz", photo_url: "images/HP/ron.jpg" }
-
 Repo.insert! %User{ first_name: "Draco", last_name: "Malfoy", email: "hp@hogwarts.wiz", photo_url: "images/HP/draco.jpg" }
 Repo.insert! %User{ first_name: "Cedric", last_name: "diggory", email: "hp@hogwarts.wiz", photo_url: "images/HP/cedricdiggory.jpg" }
 Repo.insert! %User{ first_name: "Neville", last_name: "Longbottom", email: "hp@hogwarts.wiz", photo_url: "images/HP/neville.jpg" }
 Repo.insert! %User{ first_name: "Rubeus", last_name: "Hagrid", email: "hp@hogwarts.wiz", photo_url: "images/HP/rubeus.jpg" }
+
+
+Repo.insert! %User{ first_name: "Oliver", last_name: "wood", email: "ow@hogwarts.wiz", photo_url: "images/HP/oloverwood.jpg" }
+Repo.insert! %User{ first_name: "Ginny", last_name: "Weasley", email: "gw@hogwarts.wiz", photo_url: "images/HP/ginny.jpg" }
+Repo.insert! %User{ first_name: "Luna", last_name: "Lovegood", email: "lg@hogwarts.wiz", photo_url: "images/HP/luna.jpg" }
+Repo.insert! %User{ first_name: "Percy", last_name: "Weasley", email: "pw@hogwarts.wiz", photo_url: "images/HP/percy.jpg" }
+Repo.insert! %User{ first_name: "Sirius", last_name: "Black", email: "sb@hogwarts.wiz", photo_url: "images/HP/sirius.jpg" }
+Repo.insert! %User{ first_name: "Seamus", last_name: "Finnigan", email: "sf@hogwarts.wiz", photo_url: "images/HP/seamusfinnigan.jpg" }
+Repo.insert! %User{ first_name: "Nymphadora", last_name: "Tonks", email: "nt@hogwarts.wiz", photo_url: "images/HP/nymphadora.jpg" }
+Repo.insert! %User{ first_name: "Arthur", last_name: "Weasley", email: "aw@hogwarts.wiz", photo_url: "images/HP/arthurweasley.jpg" }
+Repo.insert! %User{ first_name: "Molly", last_name: "Weasley", email: "mw@hogwarts.wiz", photo_url: "images/HP/mollyweasley.jpg" }
+Repo.insert! %User{ first_name: "Cho", last_name: "Chan", email: "cc@hogwarts.wiz", photo_url: "images/HP/cho.jpg" }
+Repo.insert! %User{ first_name: "Ginny", last_name: "Weasley", email: "gw@hogwarts.wiz", photo_url: "images/HP/ginny.jpg" }
+Repo.insert! %User{ first_name: "Fred", last_name: "Weasley", email: "fw@hogwarts.wiz", photo_url: "images/HP/fredweasley.jpg" }
+Repo.insert! %User{ first_name: "George", last_name: "Weasley", email: "george@hogwarts.wiz", photo_url: "images/HP/georgeweasley.jpg" }
